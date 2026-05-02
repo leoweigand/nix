9720492b32b426ee04d261463607d3f8901e55e9
Leo Weigand <5489276+leoweigand@users.noreply.github.com>
2026-05-02 11:59:05 +0200

    add starship prompt and zoxide to nixos zsh

--- modules/home/nixos/zsh/default.nix
+++ modules/home/nixos/zsh/default.nix
@@ -9,4 +9,17 @@
     enable = true;
     enableCompletion = true;
   };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [ "--cmd cd" ];  # Replace `cd` with zoxide's smart jump
+  };
 }
