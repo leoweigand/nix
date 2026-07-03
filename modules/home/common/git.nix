009c7eaf42ba8a44683becb424f3178e09b8d1e5
Leo Weigand <5489276+leoweigand@users.noreply.github.com>
2026-07-03 19:02:49 +0200

    make main default branch name

--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -6,6 +6,7 @@
     settings = {
       user.name = "Leo Weigand";
       user.email = "5489276+leoweigand@users.noreply.github.com";
+      init.defaultBranch = "main";
       push.default = "current";
       pull.ff = "only";
       url."git@github.com:".insteadOf = "https://github.com/";
