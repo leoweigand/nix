90f37d50461f99cef80bb133957dd8f89797e1f6
Leo Weigand <5489276+leoweigand@users.noreply.github.com>
2026-08-12 16:03:31 +0200

    chore(crusher): remove ssh workarounds

--- machines/crusher/configuration.nix
+++ machines/crusher/configuration.nix
@@ -14,10 +14,8 @@
   home-manager.users.leo.programs.ssh = {
     enable = true;
     matchBlocks."github.com" = {
-      # macOS's built-in agent exits without responding, hanging GitHub SSH.
       identityFile = "~/.ssh/id_ed25519";
       identitiesOnly = true;
-      extraOptions.IdentityAgent = "none";
     };
   };
 
